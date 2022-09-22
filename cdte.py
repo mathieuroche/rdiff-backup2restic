1d4bf06ce35434c65d4b05c1c28e32f7d8d3d956
Mathieu <mathieu@example.com>
2022-09-22 22:12:29 +0200

    Fix pylint issues

--- cdte.py
+++ cdte.py
@@ -3,10 +3,10 @@ import os
 
 logging.basicConfig(format = '%(asctime)-15s %(levelname)s %(message)s', level = logging.INFO)
 
-def change_arbo_encoding(dir, src_encoding = 'ISO-8859-1', dst_encoding = 'utf-8'):
+def change_arbo_encoding(directory, src_encoding = 'ISO-8859-1', dst_encoding = 'utf-8'):
     nb_renamed = 0
-    logging.info('Change arborescence encoding or %s (%s=>%s)' % (dir, src_encoding, dst_encoding))
-    for root, dirs, files in os.walk(dir):
+    logging.info('Change arborescence encoding of %s (%s=>%s)',directory, src_encoding, dst_encoding)
+    for root, dirs, files in os.walk(directory):
         for filename in files:
             try:
                 decoded = filename.decode(dst_encoding)
@@ -14,18 +14,18 @@ def change_arbo_encoding(dir, src_encoding = 'ISO-8859-1', dst_encoding = 'utf-8
                 decoded = filename.decode(src_encoding)
                 src = os.path.join(root, filename)
                 dst = os.path.join(root, decoded.encode(dst_encoding))
-                logging.info("Rename %s => %s" % (src, dst))
+                logging.info("Rename %s => %s", src, dst)
                 nb_renamed = nb_renamed + 1
                 os.rename(src, dst)
-                
-        for dirname in dirs:  
+
+        for dirname in dirs:
             try:
                 decoded = dirname.decode(dst_encoding)
             except UnicodeDecodeError:
                 decoded = dirname.decode(src_encoding)
                 src = os.path.join(root, dirname)
                 dst = os.path.join(root, decoded.encode(dst_encoding))
-                logging.info("Rename %s => %s" % (src, dst))
+                logging.info("Rename %s => %s" , src, dst)
                 nb_renamed = nb_renamed + 1
                 os.rename(src, dst)
                 dirs.append(decoded.encode(dst_encoding))
